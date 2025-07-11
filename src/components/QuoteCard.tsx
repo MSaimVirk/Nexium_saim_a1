80eda39ee775c0d8ca347ab0d8c00f27069d1f41
saim___virk <saim123sadiq@gmail.com>
2025-07-11 12:16:53 +0500

    added themes

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -5,7 +5,7 @@ type Quote = {
 
 export default function QuoteCard({ quote }: { quote: Quote }) {
   return (
-    <div className="bg-white rounded-xl shadow p-4 border border-gray-200">
+    <div className="bg-white rounded-xl shadow p-4 hover:border hover:border-gray-100 dark:bg-black">
       <p className="text-lg italic"> {quote.text} </p>
       <p className="text-right mt-2 font-semibold">— {quote.author || 'Unknown'}</p>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
+import { ModeToggle } from "../components/DarkModeToogle";
 
 export default function Header() {
   const pathname = usePathname();
@@ -14,23 +15,27 @@ export default function Header() {
 
   return (
     <header className="w-full border-b bg-gray-500 dark:bg-black py-6">
-      <nav className="flex items-center justify-center gap-15">
+      <nav className="flex items-center justify-between px-6">
+        <div className="flex items-center gap-4">
         {links.map(({ href, label }) => (
           <Link
             key={href}
             href={href}
             className={clsx(
-  "text-sm font-medium px-3 py-2 rounded transition-colors duration-200",
-  "hover:bg-[#f9f9f9] hover:text-black",
-  pathname === href
-    ? "bg-gray-500 text-white"
-    : "text-gray-800 dark:text-gray-300"
-)}
-
+              "text-sm font-medium px-3 py-2 rounded transition-colors duration-200",
+              "hover:bg-[#f9f9f9] hover:text-black",
+              pathname === href
+                ? "bg-gray-500 text-white"
+                : "text-gray-800 dark:text-gray-300"
+            )}
           >
             {label}
           </Link>
         ))}
+        </ div>
+        <div className="flex items-center gap-4">
+          <ModeToggle />
+        </div>
       </nav>
     </header>
   );
